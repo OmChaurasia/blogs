38b0db41a3e7d6e089346a4603c82da85c9df49d
OmChaurasia <omchaurasia272301up@gamil.com>
2021-06-20 12:50:00 -0400

    new blog added

--- js/data.js
+++ js/data.js
@@ -1,17 +1,18 @@
 data=[
+    
+    {
+        "title":"Create json api from excel spreadsheet",
+        "des":"Create JSON api from google spreadsheet || in hindi",
+        "img": "this.jpg",
+        "file": "googlesheetjson.html",
+        "categ":"Short Code"
+    },
     {
         "title":"Grid layout CSS",
         "des":"Display Grid Layout Responsive Design CSS सीखे हिंदी में || पूरा grid layout एक साथ in one page in hindi ",
         "img": "img/gridlayoutgif (2).gif",
         "file": "grid_layout.html",
         "categ":"Short Code"
-    },
-    {
-        "title":"hllo",
-        "des":"second",
-        "img": "this.jpg",
-        "file": "name.html",
-        "categ":"Computer Hardware"
     },
     {
         "title":"helo",
